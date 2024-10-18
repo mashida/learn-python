40ef7185c76c044e5a830ae450c0874d52d23ecf
Nikita Serditov <34740100+mashida@users.noreply.github.com>
2024-10-18 20:33:50 +0300

    Update task_02.py
    Задача 2024.10.18.08
    
    Выведите на экран код страны Russia вот так
    
    Страна Russia, код [RU]

--- 2024.10.18/task_02.py
+++ 2024.10.18/task_02.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 import requests  # импорт библиотеки requests
 
 LANGUAGES = ['ru', 'en']
@@ -6,13 +8,20 @@ LANGUAGES = ['ru', 'en']
 def get_list_of_available_countries() -> list[dict[str, str]]:
 
     url = 'https://date.nager.at/api/v3/AvailableCountries'
-    res = requests.get(url)
+    response = requests.get(url).json()
 
-    return res.json()
+    return response
 
 
 def main() -> None:
-    print(get_list_of_available_countries())
+    countries = get_list_of_available_countries()
+
+    for country in countries:
+        if country['name'] == 'Russia':
+            print(f'Страна Russia, код [{country["countryCode"]}]')
+            return
+
+    print(f"Страна Russia не найдена")
 
 
 if __name__ == '__main__':
