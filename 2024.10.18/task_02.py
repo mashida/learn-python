bec1f272a987c6ad31c532c73d94c53da754917a
Nikita Serditov <34740100+mashida@users.noreply.github.com>
2024-10-18 20:53:49 +0300

    Update task_02.py
    Задача 2024.10.18.08
    
    Мы получаем список словарей
    [
     {'countryCode': 'RU', 'name': 'Russia},
     {'countryCode': AD', 'name': 'Andorra'}
    ]
    Переделайте этот список в словарь
    {'Russia': 'RU', 'Andorra': 'AD'}

--- 2024.10.18/task_02.py
+++ 2024.10.18/task_02.py
@@ -12,16 +12,26 @@ def get_list_of_available_countries() -> list[dict[str, str]]:
 
     return response
 
+def get_dict_of_countries_out_of_list(c: list[dict[str, str]]):
+    result = dict()
+    for country in c:
+        result[country['name']] = country['countryCode']
+
+    return result
+
+# или однострочник
+# def get_dict_of_countries_out_of_list(c: list[dict[str, str]]):
+    # return dict((country['name'], country['countryCode']) for country in c)
+
 
 def main() -> None:
-    countries = get_list_of_available_countries()
+    list_of_countries = get_list_of_available_countries()
+    dict_of_countries = get_dict_of_countries_out_of_list(list_of_countries)
 
-    for country in countries:
-        if country['name'] == 'Russia':
-            print(f'Страна Russia, код [{country["countryCode"]}]')
-            return
-
-    print(f"Страна Russia не найдена")
+    if 'Russia' in dict_of_countries:
+        print(f'Страна Russia, код [{dict_of_countries["Russia"]}]')
+    else:
+        print(f"Страна Russia не найдена")
 
 
 if __name__ == '__main__':
