42fd9d03a41bd8e04a82d26cec02a77952338dec
Nikita Serditov <34740100+mashida@users.noreply.github.com>
2024-10-18 21:17:20 +0300

    Update task_02.py
    added type hinting

--- 2024.10.18/task_02.py
+++ 2024.10.18/task_02.py
@@ -1,15 +1,18 @@
 import requests  # импорт библиотеки requests
 
+DictOfCountry = dict[str, str]
+ListOfCountries = list[DictOfCountry]
 
-def get_list_of_available_countries() -> list[dict[str, str]]:
+
+def get_list_of_available_countries() -> ListOfCountries:
     url = 'https://date.nager.at/api/v3/AvailableCountries'
-    response = requests.get(url).json()
+    response: ListOfCountries = requests.get(url).json()
 
     return response
 
 
-def get_dict_of_countries_out_of_list(c: list[dict[str, str]]):
-    result = dict()
+def get_dict_of_countries_out_of_list(c: list[dict[str, str]]) -> DictOfCountry:
+    result: DictOfCountry = dict()
     for country in c:
         result[country['name']] = country['countryCode']
 
@@ -22,8 +25,8 @@ def get_dict_of_countries_out_of_list(c: list[dict[str, str]]):
 
 
 def main() -> None:
-    list_of_countries = get_list_of_available_countries()
-    dict_of_countries = get_dict_of_countries_out_of_list(list_of_countries)
+    list_of_countries: ListOfCountries = get_list_of_available_countries()
+    dict_of_countries: DictOfCountry = get_dict_of_countries_out_of_list(list_of_countries)
 
     if 'Russia' in dict_of_countries:
         print(f'Страна Russia, код [{dict_of_countries["Russia"]}]')
